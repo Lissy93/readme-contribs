5fc3f127833d505e73a3bce96aff953eae155a35
Alicia Sykes <alicia@omg.lol>
2024-06-22 23:28:26 +0100

    feat: Adds GraphQL query for fetching sponsors

--- api/fetch-users.ts
+++ api/fetch-users.ts
@@ -23,10 +23,82 @@ export const fetchContributors = async (owner: string, repo: string, limit: numb
   }));
 }
 
-export const fetchSponsors = async (author: string): Promise<User[]> => {
+const fallbackFetchSponsors = async (author: string): Promise<User[]> => {
   const response = await fetch(`https://github-sponsors.as93.workers.dev/${author}`)
   if (!response.ok) {
     throw new Error(`GitHub API returned a ${response.status} ${response.statusText || 'Unknown Error'}`);
   }
   return await response.json()
 }
+
+export const fetchSponsors = async (username: string): Promise<User[]> => {
+  if (!process.env.GITHUB_TOKEN) {
+    return fallbackFetchSponsors(username);
+  }
+
+  const query = `
+    query {
+      user(login: "${username}") {
+        sponsorshipsAsMaintainer(first: 100) {
+          edges {
+            node {
+              sponsorEntity {
+                ... on User {
+                  login
+                  name
+                  avatarUrl: avatarUrl(size: 100)
+                }
+                ... on Organization {
+                  login
+                  name
+                  avatarUrl: avatarUrl(size: 100)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const headers = {
+    'Authorization': `bearer ${process.env.GITHUB_TOKEN}`,
+    'Content-Type': 'application/json',
+    'User-Agent': 'GitHub GraphQL API'
+  };
+
+  try {
+    const response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify({ query })
+    });
+
+    // If request failed, try using my REST API as a fallback
+    if (!response.ok) {
+      try {
+        return fallbackFetchSponsors(username)
+      } catch (fallbackError) {
+        throw new Error(
+          `Failed to fetch GitHub data with status ${response.status}; `
+          + `Fallback also failed with ${fallbackError}`
+        );
+      }
+    }
+
+    const data = await response.json();
+    if (!data.data.user) {
+      throw new Error(`User ${username} not found or has no sponsors`);
+    }
+    return data.data.user.sponsorshipsAsMaintainer.edges.map((edge: { node: { sponsorEntity: any; }; }) => {
+      const entity = edge.node.sponsorEntity;
+      return {
+        login: entity.login || null,
+        name: entity.name,
+        avatarUrl: entity.avatarUrl
+      };
+    });
+  } catch (error) {
+    throw new Error(`Failed to fetch sponsors: ${error};`);
+  }
+};
