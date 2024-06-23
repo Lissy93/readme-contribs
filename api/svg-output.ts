e07bf75a78da5fbd7560e69a3761e9cce28d2e1a
Alicia Sykes <alicia@omg.lol>
2024-06-23 13:19:30 +0100

    feat: Make users hyperlinks

--- api/svg-output.ts
+++ api/svg-output.ts
@@ -39,7 +39,6 @@ export const createErrorSVG = (message: string, options: SvgOptions) => {
  * Given an array of GitHub users, and the SVG options, generate an SVG string.
  */
 export const createUserSVG = (users: User[], options: SvgOptions): string => {
-  // User-specified SVG options
   const {
     title,
     avatarSize,
@@ -54,54 +53,52 @@ export const createUserSVG = (users: User[], options: SvgOptions): string => {
     limit,
   } = options;
 
-  // Derived values
-  const maxTextWidth = avatarSize; // Max width of text to fit under the avatar
-  const rowHeight = avatarSize + textOffset + margin; // Total height of a row
+  const maxTextWidth = avatarSize;
+  const rowHeight = avatarSize + textOffset + margin;
   const pathRounding = shape === 'circle' ? '50%' : (shape === 'squircle' ? '25%' : '0%');
-  const titleFontSize = Number(fontSize) * 2; // Larger font size for the title
-  const titleHeight = title ? titleFontSize * 1.5 : 0; // Additional height if there is a title
+  const titleFontSize = Number(fontSize) * 2;
+  const titleHeight = title ? titleFontSize * 1.5 : 0;
   const numberToDisplay = Number(limit > 0 ? limit : users.length);
 
   let svgContent = users.map((user, index) => {
-    // Calculate x and y positions based on index, adjusted for title height if present
     let x = margin + (index % perRow) * (avatarSize + margin);
     let y = margin + Math.floor(index / perRow) * rowHeight + titleHeight;
-
+    const profileUrl = `https://github.com/${user.login}`;
     const escapedAvatarUrl = user.avatarUrl.replace(/&/g, '&amp;');
-    let displayName = user.name || user.login; // Fallback to login if name is not available
+    let displayName = user.name || user.login;
     const escapedName = escapeXml(displayName);
 
-    // Estimate text length and append ellipsis if it's likely to overflow
-    const characterWidthEstimate = fontSize * 0.4; // Roughly estimate the width of a character
+    const characterWidthEstimate = fontSize * 0.4;
     const maxCharacters = Math.floor(maxTextWidth / characterWidthEstimate);
     if (escapedName.length > maxCharacters) {
-      displayName = escapedName.substring(0, maxCharacters - 2) + '…'; // Truncate and add ellipsis
+      displayName = escapedName.substring(0, maxCharacters - 2) + '…';
     }
 
     return `
-      <image
-        href="${escapedAvatarUrl}"
-        x="${x}"
-        y="${y}"
-        height="${avatarSize}px"
-        width="${avatarSize}px"
-        clip-path="inset(0% round ${pathRounding})"
-      />
-      <text
-        x="${x}"
-        y="${y + avatarSize + textOffset}"
-        font-family="${fontFamily}"
-        font-size="${fontSize}px"
-        fill="${textColor}"
-      >
-        ${displayName}
-      </text>
+      <a xlink:href="${profileUrl}" target="_blank">
+        <image
+          href="${escapedAvatarUrl}"
+          x="${x}"
+          y="${y}"
+          height="${avatarSize}px"
+          width="${avatarSize}px"
+          clip-path="inset(0% round ${pathRounding})"
+        />
+        <text
+          x="${x}"
+          y="${y + avatarSize + textOffset}"
+          font-family="${fontFamily}"
+          font-size="${fontSize}px"
+          fill="${textColor}"
+        >
+          ${displayName}
+        </text>
+      </a>
     `;
   })
   .slice(0, numberToDisplay)
   .join('')
 
-  // Title SVG element if title is specified
   const titleSvg = title ? `
     <text
       x="50%"
@@ -116,15 +113,12 @@ export const createUserSVG = (users: User[], options: SvgOptions): string => {
     </text>
   ` : '';
 
-  // Calculate the total height based on the number of users
   let totalRows = Math.ceil(users.length / perRow);
   let svgHeight = totalRows * rowHeight + margin + titleHeight;
-
-  // Calculate the width of the SVG
   let svgWidth = perRow * (avatarSize + margin) + margin;
 
   return `
-    <svg width="${svgWidth}" height="${svgHeight}" xmlns="http://www.w3.org/2000/svg">
+    <svg width="${svgWidth}" height="${svgHeight}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <rect width="100%" height="100%" fill="${backgroundColor}"/>
       ${titleSvg}
       ${svgContent}
